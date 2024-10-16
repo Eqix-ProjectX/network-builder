c9aea0095bfeaafaacf0f5e3149f19e8f5d730ed
EQIX-TAM-JP <snakayama@ap.equinix.com>
2024-10-16 18:22:50 +0900

    add Darshan's module for ne<>aws

--- main.tf
+++ main.tf
@@ -6,13 +6,16 @@ terraform {
     iosxe = {
       source = "CiscoDevNet/iosxe"
     }
+    aws = {
+      source = "hashicorp/aws"
+    }
   }
   cloud {
 
     organization = "EQIX_projectX"
 
     workspaces {
-      name = "network_builder_apac"
+      name = "network-builder-apac"
     }
   }
 }
@@ -50,10 +53,10 @@ data "terraform_remote_state" "bgp" {
 }
 
 locals {
-  ipv4_vrf_pri   = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges[1], 1)
-  ipv4_vrf_sec   = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges_sec[1], 1)
-  ipv4_int_pri      = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges[1], 2)
-  ipv4_int_sec      = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges_sec[1], 2)
+  ipv4_vrf_pri = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges[1], 1)
+  ipv4_vrf_sec = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges_sec[1], 1)
+  ipv4_int_pri = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges[1], 2)
+  ipv4_int_sec = cidrhost(data.terraform_remote_state.bgp.outputs.vrf_ranges_sec[1], 2)
 }
 
 # IOS-XE configuration
@@ -251,3 +254,120 @@ resource "equinix_metal_virtual_circuit" "peer_sec" {
   depends_on = [time_sleep.wait_2_min]
 }
 */
+
+# ##vipin - module to spin up FCR
+# data "terraform_remote_state" "fcr_id" {
+#   backend = "remote"
+
+#   config = {
+#     organization = "EQIX_projectX"
+#     workspaces = {
+#       name = "network-apac"
+#     }
+#   }
+# }
+
+# module "cloud_router_aws_connection" {
+#   source = "equinix/fabric/equinix//modules/cloud-router-connection"
+
+#   connection_name       = var.connection_name
+#   connection_type       = var.connection_type
+#   notifications_type    = var.notifications_type
+#   notifications_emails  = var.emails
+#   bandwidth             = var.bandwidth
+#   purchase_order_number = var.purchase_order_number
+
+#   #Aside
+#   aside_fcr_uuid = "fc17e67a-d58c-458d-b1cb-840121701561"
+
+#   #Zside
+#   zside_ap_type               = "SP"
+#   # zside_ap_authentication_key = var.authentication_key
+#   zside_ap_profile_type       = "L2_PROFILE"
+#   zside_location              = var.awslocation
+#   zside_seller_region         = var.seller_region
+#   zside_fabric_sp_name        = "AWS Direct Connect"
+# }
+
+# data "aws_dx_connection" "aws_connection" {
+#   name = var.connection_name
+#   depends_on = [
+#     module.cloud_router_aws_connection
+#   ]
+# }
+
+# resource "aws_dx_connection_confirmation" "confirmation" {
+#   connection_id = data.aws_dx_connection.aws_connection.id
+# }
+
+# resource "aws_dx_gateway" "aws_gateway" {
+#   depends_on = [
+#     module.cloud_router_aws_connection
+#   ]
+#   name            = var.aws_gateway_name
+#   amazon_side_asn = var.aws_gateway_asn
+# }
+
+# resource "aws_dx_private_virtual_interface" "aws_virtual_interface" {
+#   depends_on = [
+#     module.cloud_router_aws_connection,
+#     aws_dx_gateway.aws_gateway
+#   ]
+#   lifecycle {
+#     precondition {
+#       condition     = can(regex("^dxcon-*", data.aws_dx_connection.aws_connection.id))
+#       error_message = "connection must be ready before running"
+#     }
+#   }
+#   connection_id    = data.aws_dx_connection.aws_connection.id
+#   name             = var.aws_vif_name
+#   vlan             = data.aws_dx_connection.aws_connection.vlan_id
+#   address_family   = var.aws_vif_address_family
+#   bgp_asn          = var.aws_vif_bgp_asn
+#   amazon_address   = var.aws_vif_amazon_address
+#   customer_address = var.aws_vif_customer_address
+#   bgp_auth_key     = var.md5
+#   dx_gateway_id    = aws_dx_gateway.aws_gateway.id
+# }
+
+# module "routing_protocols" {
+#   source = "equinix/fabric/equinix//modules/cloud-router-routing-protocols"
+
+#   connection_uuid = data.aws_dx_connection.aws_connection.id
+
+#   # Direct RP Details
+#   direct_rp_name         = var.direct_rp_name
+#   direct_equinix_ipv4_ip = cidrhost(local.range_aws, 1)
+
+#   # BGP RP Details
+#   bgp_rp_name            = var.bgp_rp_name
+#   bgp_customer_asn       = var.bgp_customer_asn
+#   bgp_customer_peer_ipv4 = cidrhost(local.range_aws, 2)
+#   bgp_enabled_ipv4       = var.bgp_enabled_ipv4
+# }
+
+module "aws-connection" {
+  source                = "github.com/Eqix-ProjectX/terraform-equinix-virtualconnection-aws/"
+  connection_name       = var.connection_name_aws
+  connection_type       = var.connection_type
+  notifications_emails  = var.emails
+  bandwidth             = var.bandwidth_aws
+  purchase_order_number = var.purchase_order_number
+  device_uuid           = data.terraform_remote_state.ne.outputs.vd_uuid
+  interface_number      = var.interface_number_aws
+  authentication_key    = var.authentication_key_aws
+  profile_uuid          = var.profile_uuid_aws
+  metro                 = var.metro_code
+  aws_vpc_cidr          = var.aws_vpc_cidr
+  seller_region         = var.seller_region
+  aws_region            = var.aws_region
+  aws_vpc_name          = var.aws_vpc_name
+  aws_subnet_name       = var.aws_subnet_name
+  aws_subnet_cidr       = var.aws_subnet_cidr
+  aws_vpg_name          = var.aws_vpg_name
+  aws_vif_name          = var.aws_vif_name
+  customer_asn          = var.vnf_asn
+  aws_bgp_auth_key      = var.aws_bgp_auth_key
+  amazon_ip_address     = var.amazon_ip_address
+  customer_ip_address   = var.customer_ip_address
+}
--- variables.tf
+++ variables.tf
@@ -12,3 +12,87 @@ variable "pri_vc" {}
 variable "sec_vc" {}
 variable "emails" {}
 variable "connection_name" {}
+
+#vipin - aws details 
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
+# variable "profile_uuid" {}
+# variable "authentication_key" {}
+#vipin ecx details 
+# variable "ecx_access_key" {}
+# variable "ecx_secret_key" {}
+# variable "ecx_auth_token" {}
+# variable "FCRtoAWSspeed" {}
+# variable "FCRpurchaseorder" {}
+# variable "FCRmetrocode" {}
+# variable "FCRaccountnumber" {}
+# variable "FCRprojectid" {}
+# variable "email" {}
+variable "purchase_order_number" {}
+# variable "FCRRoutername" {}
+# variable "FCRlocation" {}
+# variable "FCRemail" {}
+
+variable "notifications_type" {
+  description = "Notification Type - ALL is the only type currently supported"
+  type        = string
+  default     = "ALL"
+}
+variable "bandwidth" {
+  description = "Connection bandwidth in Mbps"
+  type        = number
+}
+# variable "zside_ap_authentication_key" {
+#   description = "Authentication key for provider based connections"
+#   type        = string
+#   sensitive   = true
+# }
+# variable "zside_location" {
+#   description = "Access point metro code"
+#   type        = string
+#   default     = "SP"
+# }
+# variable "zside_seller_region" {
+#   description = "Access point seller region"
+#   type        = string
+#   default     = ""
+# }
+
+#fcr routing template
+variable "direct_rp_name" {
+  description = "Name of the Direct Routing Protocol"
+  type        = string
+}
+variable "bgp_rp_name" {
+  description = "Name of the BGP Routing Protocol"
+  type        = string
+  default     = ""
+}
+variable "bgp_enabled_ipv4" {
+  description = "Boolean Enable Flag for IPv4 Peering on BGP Routing Protocol"
+  type        = bool
+  default     = true
+}
+variable "bgp_customer_asn" {
+  description = "Customer ASN for BGP Routing Protocol"
+  type        = string
+  default     = ""
+}
+
+variable "connection_name_aws" {}
+variable "connection_type" {}
+variable "interface_number_aws" {}
+variable "authentication_key_aws" {}
+variable "profile_uuid_aws" {}
+variable "seller_region" {}
+variable "aws_region" {}
+variable "bandwidth_aws" {}
+variable "aws_vpc_name" {}
+variable "aws_vpc_cidr" {}
+variable "aws_subnet_name" {}
+variable "aws_subnet_cidr" {}
+variable "aws_vpg_name" {}
+variable "aws_vif_name" {}
+variable "aws_bgp_auth_key" {}
+variable "amazon_ip_address" {}
+variable "customer_ip_address" {}
