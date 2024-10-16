0f161d6873df5ab9445889883e475d0efaaaf2c1
Vipin Vikraman <vvikraman@ap.equinix.com>
2024-10-16 18:08:36 +0800

    to add AWS outputs

--- outputs.tf
+++ outputs.tf
@@ -30,3 +30,29 @@ output "token_sec" {
   value     = equinix_metal_connection.vrf2vd.service_tokens[1].id
   sensitive = true
 }*/
+
+
+# for AWS connection
+output "fcr_2_aws_vc_id" {
+  value = equinix_fabric_connection.Localname_fcr2aws.id
+}
+
+output "AWS_DX_id" {
+value = data.aws_dx_connection.aws_connection.id
+}
+
+output "AWS_VPG_id" {
+  value = aws_vpn_gateway.vgw.id
+}
+
+output "AWS_VPC_id" {
+  value = aws_vpc.main.id
+}
+
+output "AWS_Subnet_id" {
+  value = aws_subnet.private.id
+}
+
+output "AWS_VIF_VLAN_id" {
+  value = data.aws_dx_connection.aws_connection.vlan_id
+}
